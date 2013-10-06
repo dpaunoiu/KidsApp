a19a22228d01f9dea9b90189a80ea4f1b68ca1c8
dpaunoiu <dragos@Dragoss-MacBook-Air.local>
2013-10-06 21:54:29 +0300

    Initial Commit

---
+++ KidsApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  KidsApp
+//
+//  Created by Dragos on 10/6/13.
+//  Copyright (c) 2013 Dragos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ KidsApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  KidsApp
+//
+//  Created by Dragos on 10/6/13.
+//  Copyright (c) 2013 Dragos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
